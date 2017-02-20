69f5216fe30129f07b03a3e97bb2f8f4c4388799
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-02-20 12:24:11 -0600

    First Pass

---
+++ Assets/CamGyroAR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CamGyroAR : MonoBehaviour 
+{
+	public GameObject cameraViewport;
+	GameObject camParent;
+
+	void Start () 
+	{
+		camParent = new GameObject ("CamParent");
+		GetComponent<Camera>().transform.position = this.transform.position;
+		this.transform.parent = camParent.transform;
+		camParent.transform.Rotate (Vector3.right, 90);
+		Input.gyro.enabled = true;
+
+		WebCamTexture webCamTexture = new WebCamTexture ();
+		cameraViewport.GetComponent<MeshRenderer> ().material.mainTexture = webCamTexture;
+		webCamTexture.Play ();
+	}
+
+	void Update () 
+	{
+		Quaternion rotationFix = new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+		this.transform.localRotation = rotationFix;
+	}
+}
---
+++ Assets/Zoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour 
+{
+	float previousDistance;
+	float zoomSpeed = 1.0f;
+
+	void Update () 
+	{
+		if (Input.touchCount == 2 && (Input.GetTouch (0).phase == TouchPhase.Began ||
+		    Input.GetTouch (1).phase == TouchPhase.Began)) {
+			//calibrate the distance
+			previousDistance = Vector2.Distance (Input.GetTouch (0).position, Input.GetTouch (1).position);
+		} else if (Input.touchCount == 2 && (Input.GetTouch (0).phase == TouchPhase.Moved ||
+		         Input.GetTouch (1).phase == TouchPhase.Moved)) 
+		{
+			float distance;
+			Vector2 touch1 = Input.GetTouch (0).position;
+			Vector2 touch2 = Input.GetTouch (1).position;
+
+			distance = Vector2.Distance (touch1, touch2);
+
+			//camera based on pinch/zoom
+			float pinchAmount = (previousDistance - distance) * zoomSpeed * Time.deltaTime;
+			pinchAmount = Mathf.Clamp (pinchAmount, -10, 5);
+			transform.Translate (0, 0, -pinchAmount);
+			previousDistance = distance;
+		}
+	}
+}
