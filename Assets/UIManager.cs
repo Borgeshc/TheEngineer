1fb291ee7ec5cd955cb0a84721aa26036d587282
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-05 17:55:53 -0600

    Began work on ui

---
+++ Assets/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	public KeyCode inventoryKeyCode;
+	public GameObject inventory;
+	public AudioClip inventorySound;
+
+	AudioSource source;
+
+	void Start () 
+	{
+		source = GetComponent<AudioSource> ();
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (inventoryKeyCode)) 
+		{
+			source.clip = inventorySound;
+			source.Play ();
+			inventory.SetActive (!inventory.activeSelf);
+		}
+	}
+}
