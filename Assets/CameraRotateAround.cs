fd2f5f5d091fc62970ab8b2314be2951a8b3bb97
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-16 12:31:20 -0500

    Added the main menu

---
+++ Assets/CameraRotateAround.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour 
+{
+	public float speed;
+	public GameObject anchor;
+
+	void Update ()
+	{
+		transform.LookAt (anchor.transform);
+		transform.Rotate (Vector3.right * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour 
+{
+	public void NewGame()
+	{
+		SceneManager.LoadScene ("World");
+	}
+
+	public void Continue()
+	{
+		SceneManager.LoadScene ("World");
+	}
+
+	public void Settings()
+	{
+		
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
